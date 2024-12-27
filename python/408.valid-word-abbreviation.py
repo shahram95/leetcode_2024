17e7af79b476bc8d5a207b6c3e7be8269de371ab
shahram95 <shahramnajam.neduet@gmail.com>
2024-12-27 02:50:14 -0500

    Added 408. Valid word abbreviation

---
+++ python/408.valid-word-abbreviation.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=408 lang=python3
+#
+# [408] Valid Word Abbreviation
+#
+
+# @lc code=start
+class Solution:
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        abbr_idx = 0
+        word_idx = 0
+
+        while word_idx < len(word) and abbr_idx < len(abbr):
+            if abbr[abbr_idx].isdigit():
+                if abbr[abbr_idx] == '0':
+                    return False
+                num = 0
+                while abbr_idx < len(abbr) and abbr[abbr_idx].isdigit():
+                    num = num*10 + int(abbr[abbr_idx])
+                    abbr_idx += 1
+                word_idx += num
+            else:
+                if len(word) < word_idx or word[word_idx] != abbr[abbr_idx]:
+                    return False
+                word_idx+= 1
+                abbr_idx+= 1
+        return len(word) == word_idx and len(abbr) == abbr_idx
+# @lc code=end
+
