2785bbcdf1927dbf6192456cc01633132c379f5d
shahram95 <shahramnajam.neduet@gmail.com>
2024-12-28 06:09:37 -0500

    Added 539. Minimum time difference

---
+++ python/539.minimum-time-difference.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=539 lang=python3
+#
+# [539] Minimum Time Difference
+#
+
+# @lc code=start
+class Solution:
+    def findMinDifference(self, timePoints: List[str]) -> int:
+        minutes = [int(time[:2])*60 + int(time[3:]) for time in timePoints]
+        minutes.sort()
+        ans = min(minutes[i+1] - minutes[i] for i in range(len(minutes)-1))
+        return min(ans, 24*60 - minutes[-1] + minutes[0])
+# @lc code=end
+
