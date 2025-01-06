e024e8a72abbd25fdd2120ba3b94e5204e5b309c
shahram95 <shahramnajam.neduet@gmail.com>
2025-01-06 09:53:01 -0500

    Added 3 solutions for 1. Two Sum

--- python/1.two-sum.py
+++ python/1.two-sum.py
@@ -7,6 +7,31 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        '''
+        Method # 1: Brute Force
+        '''
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] == target - nums[j]:
+        #             return [i,j]
+        # return []
+
+        '''
+        Method # 2: Two pass hashtable
+        '''
+        # hashmap = dict()
+
+        # for i in range(len(nums)):
+        #     hashmap[nums[i]] = i
+        # for i in range(len(nums)):
+        #     comp = target - nums[i]
+        #     if comp in hashmap and hashmap[comp] != i:
+        #         return [hashmap[comp], i]
+        # return []
+
+        '''
+        Method # 3: One pass hashtable
+        '''
         idx_dict = dict()
 
         for idx,num in enumerate(nums):
